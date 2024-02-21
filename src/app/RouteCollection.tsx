5915205fb7539862add7a170ca21b81b183e2a92
rawera_daniel <rawera.daniel@gmail.com>
2024-02-21 15:46:28 +0300

    feat: Implement page not found for non-existent pages

--- src/app/RouteCollection.tsx
+++ src/app/RouteCollection.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/pages/Home";
 import AboutPage from "./views/pages/AboutPage";
+import NotFoundPage from "./views/pages/NotFoundPage";
+import { Container } from "@mui/material";
 
 const RouteCollection = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutPage />} />
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Container>
   );
 };
 
